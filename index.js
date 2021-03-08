40068a791f10fb24dd007b9a7c7693be88905dd3
ziitania <tjhaweszii@gmail.com>
2021-03-07 19:14:05 -0800

    Mission Complete

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Write your solution in this file!
+
+const employee = {
+    name: 'Amiya',
+    streetAddress: 'Rhodes Island Command Center'
+};
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    const newEmploy = {...employee};
+    newEmploy[key] = value;
+    return newEmploy;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee;
+}
+
+function deleteFromEmployeeByKey(employee, key) {
+    const newEmploy = {...employee};
+    delete newEmploy[key];
+    return newEmploy;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+    return employee;
+}
